07cbb349bb11479f19a8f374d2e1a4378daa7bda
arianaporroche <874055@unizar.es>
2026-08-03 10:12:11 +0200

    Update navigation to Login page from Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,17 @@
+import { useNavigate } from 'react-router-dom';
 import './Home.css';
 import Button from '../../components/Button/Button';
 
 export default function Home() {
+  const navigate = useNavigate();
+
   return (
     <div className="home-page">
       <h1 className='title'>Social Numéricco</h1>
       <p className='subtitle'>La red social de los empleados. Un espacio dedicado a la colaboración.</p>
       <Button
         variant="primary"
-        onClick={null}
+        onClick={() => navigate('/login')}
       >
         Iniciar sesión
       </Button>
