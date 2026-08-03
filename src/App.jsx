477094f16c4bc0d123ffda6027388342d7fdfb97
arianaporroche <874055@unizar.es>
2026-08-03 09:54:26 +0200

    Update Home page with background, title and subtitle

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home/Home'
 import Tab1 from './pages/Tab1/Tab1'
@@ -6,19 +6,28 @@ import Tab2 from './pages/Tab2/Tab2'
 import Tab3 from './pages/Tab3/Tab3'
 import NavBar from './components/NavBar/NavBar'
 
+function Layout() {
+  return (
+    <div className="layout-container">
+      <NavBar />
+      <div className="tab-container">
+        <Outlet />
+      </div>
+    </div>
+  )
+}
+
 function App() {
   return (
     <Router>
-      <NavBar />
-
-      <div className="tab-container">
-        <Routes>
-          <Route path="/" element={<Home />} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route element={<Layout />}>
           <Route path="/tab1" element={<Tab1 />} />
           <Route path="/tab2" element={<Tab2 />} />
           <Route path="/tab3" element={<Tab3 />} />
-        </Routes>
-      </div>
+        </Route>
+      </Routes>
     </Router>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,13 +4,13 @@ import Button from '../../components/Button/Button';
 export default function Home() {
   return (
     <div className="home-page">
-      <h1 className='title'>TITLE</h1>
-      <p className='subtitle'>Subtitle</p>
+      <h1 className='title'>Social Numéricco</h1>
+      <p className='subtitle'>La red social de los empleados. Un espacio dedicado a la colaboración.</p>
       <Button
         variant="primary"
         onClick={null}
       >
-        See more
+        Iniciar sesión
       </Button>
     </div>
   );
